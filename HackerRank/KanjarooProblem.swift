38d91aca1c0106d0ce8bdcaed8d20739c6d6e7d3
Abdul Basit <abdul.basit@veripark.com.pk>
2019-11-23 15:42:11 +0500

    Kanjaroo problem Student problem

--- HackerRank/KanjarooProblem.swift
+++ HackerRank/KanjarooProblem.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class KanjarooProblem {
+    
+    func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
+        
+        var startingCount = 0
+        
+        if x1 < x2 {
+            startingCount = x1
+        }
+        else {
+            startingCount = x2
+        }
+        var distaceForKangroo1 = x1
+        var distaceForKangroo2 = x2
+        
+        for _ in startingCount...9999 {
+            
+            distaceForKangroo1 += v1
+            
+            distaceForKangroo2 += v2
+            
+            if distaceForKangroo1 == distaceForKangroo2 {
+                return "YES"
+                
+            }
+
+        }
+        return "NO"
+    }
+}
--- HackerRank/StudentSortedGradesSolution.swift
+++ HackerRank/StudentSortedGradesSolution.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class StudentSortedGradesSolution {
+    func gradeStudent (grades : [Int]) {
+        
+        var sortedGrades = [Int]()
+        
+        
+        for grade in grades  {
+            
+            var grade = grade
+            let diff = grade % 5
+            if diff >= 3 && grade >= 38 {
+                grade += 5 - diff
+            }
+            sortedGrades.append(grade)
+        }
+    }
+}
